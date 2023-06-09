ec3f18fbd85420ac1877f00c65d71125e9300458
Kishan Rekhadia <kishanrekhadia79@gmail.com>
2023-06-09 18:20:56 +0530

    React info added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 function App() {
   return (
-    <nav>
-      <h1>Bob's Bistro</h1>
+    <div>
+      <img src="./img/react-logo.png" width="40px" alt="A React logo" />
+      <h1>Fun facts about React</h1>
       <ul>
-        <li>Menu</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </nav>
+    </div>
   );
 }
 
